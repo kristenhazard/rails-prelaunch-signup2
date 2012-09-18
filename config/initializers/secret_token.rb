5294209f33d196968483049fd70795f3d5bd3a6f
Kristen Hazard <kristen@hazardbio.com>
2012-09-18 14:00:59 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPrelaunchSignup2::Application.config.secret_token = '82cb28e8d3adbe0afeff97b5c5ff4396ead48a6635b548334c3efaa12868f4f03bbb66ff3fa2091e5b47ead629daac517753a0cbb5e565c2cef97c2e980023d7'
